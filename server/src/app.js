ed85e45e57a6a69bf976e599306d50ca5a561a14
TETSUKER <roman_tokmin@mail.ru>
2024-02-13 21:40:55 +0700

    setup base server directory

---
+++ server/src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Not found" });
+});
+
+export default app;
